3f2836667af0ce3424bccc6933ad2478db03dd91
Zaz Brown <zazbrown@yandex.com>
2015-12-15 17:00:06 +0000

    Remove ability to limit paths by a second weight

--- dijkstra.py
+++ dijkstra.py
@@ -7,7 +7,6 @@ class Digraph(object):
         self.nodes = set()
         self.neighbours = defaultdict(set)
         self.dist = {}
-        self.odist = {}
 
     def addNode(self, *nodes):
         [self.nodes.add(n) for n in nodes]
@@ -16,16 +15,13 @@ class Digraph(object):
         self.addNode(frm, to)
         self.neighbours[frm].add(to)
         self.dist[ frm, to ] = d
-        self.odist[ frm, to ] = o
 
-    def dijkstra(self, start, maxD=1e309, maxDO=1e309):
+    def dijkstra(self, start, maxD=1e309):
         """Returns a map of nodes to distance from start and a map of nodes to
         the neighbouring node that is closest to start."""
         # total distance from origin
         tdist = defaultdict(lambda: 1e309)
         tdist[start] = 0
-        tOdist = defaultdict(lambda: 1e309)
-        tOdist[start] = 0
         # neighbour that is nearest to the origin
         preceding_node = {}
         unvisited = self.nodes
@@ -37,18 +33,16 @@ class Digraph(object):
             unvisited.remove(min_node)
 
             for neighbour in self.neighbours[min_node]:
-                d  =  tdist[min_node] +  self.dist[min_node, neighbour]
-                dO = tOdist[min_node] + self.odist[min_node, neighbour]
-                if tdist[neighbour] > d and maxD >= d and maxDO >= dO:
+                d = tdist[min_node] + self.dist[min_node, neighbour]
+                if tdist[neighbour] > d and maxD >= d:
                     tdist[neighbour] = d
-                    tOdist[neighbour] = dO
                     preceding_node[neighbour] = min_node
 
         return tdist, preceding_node
 
-    def min_path(self, start, end, maxD=1e309, maxDO=1e309):
+    def min_path(self, start, end, maxD=1e309):
         """Returns the minimum distance and path from start to end."""
-        tdist, preceding_node = self.dijkstra(start, maxD, maxDO)
+        tdist, preceding_node = self.dijkstra(start, maxD)
         dist = tdist[end]
         backpath = [end]
         try:
